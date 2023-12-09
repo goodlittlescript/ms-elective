7920f1f3b8762237a552bd3a236c9c3f9a08d23d
Simon Chiang <simon.a.chiang@gmail.com>
2023-12-09 09:28:36 -0700

    Add day 1

---
+++ day1/quiz.py
@@ -0,0 +1,19 @@
+# Quiz Game
+print("What is your name?")
+name = input()
+
+print("Hi " + name + ", lets play a game!")
+
+print("What state are you in?")
+response = input()
+if response == "Colorado":
+    print("You win!")
+else:
+    print("Sorry no, the answer is: Colorado")
+
+print("What school are you in?")
+response = input()
+if response == "Littleton Academy":
+    print("You win!")
+else:
+    print("Sorry no, the answer is: Littleton Academy")
