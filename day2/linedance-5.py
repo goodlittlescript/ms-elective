e51621ba3818a997744598585beb3097197e2a7e
Simon Chiang <simon.a.chiang@gmail.com>
2023-12-09 09:28:52 -0700

    Add day 2

---
+++ day2/linedance-5.py
@@ -0,0 +1,24 @@
+import os
+import time
+
+lines = [
+    ".X.......",
+    "..X......",
+    "...X.....",
+    "....X....",
+    ".....X...",
+    "......X..",
+    ".......X.",
+    "......X..",
+    ".....X...",
+    "....X....",
+    "...X.....",
+    "..X......",
+    ".X.......",
+]
+
+for _ in range(0,3):
+    for line in lines:
+        os.system('clear')
+        print(line)
+        time.sleep(0.1)
---
+++ day2/linedance-6.py
@@ -0,0 +1,27 @@
+import os
+import time
+
+lines = [
+    ".X.......",
+    "..X......",
+    "...X.....",
+    "....X....",
+    ".....X...",
+    "......X..",
+    ".......X.",
+    "......X..",
+    ".....X...",
+    "....X....",
+    "...X.....",
+    "..X......",
+    # Removed the last one...
+]
+
+for _ in range(0,3):
+    for line in lines:
+        os.system('clear')
+        print(line)
+        time.sleep(0.1)
+
+os.system('clear')
+print(lines[0])
