77a49473f157cf654465f5f5358875540f620f61
Simon Chiang <simon.a.chiang@gmail.com>
2024-01-17 19:46:32 -0700

    Add day2

---
+++ day2/linedance-4.py
@@ -0,0 +1,32 @@
+import os
+import time
+
+lines = [
+    ".X.......",
+    "..X......",
+    "...X.....",
+    "....X....",
+    ".....X...",
+    "......X..",
+    ".......X.",
+    "......X..",
+    ".....X...",
+    "....X....",
+    "...X.....",
+    "..X......",
+    ".X.......",
+]
+
+# returns the current time in seconds
+start = time.time()
+
+for line in lines:
+    os.system("clear")
+    print(line)
+    input()
+
+# so we can calculate the elapsed time
+finish = time.time()
+elapsed = finish - start
+
+print("You won in: " + str(elapsed) + "s")
